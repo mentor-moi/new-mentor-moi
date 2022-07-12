369a2b1296a2ee3fb30910869b2df8dad3520d44
mentor-moi <mentor.moi.22@gmail.com>
2022-07-12 22:39:47 +0200

    init

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test'
 test('should navigate to the about page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto('https://new-mentor-moi.vercel.app/')
-  // Find an element with the text 'About Page' and click on it
+  // Find an element with the text 'About Page' and click on itt
   await page.click('text=About')
   // The new url should be "/about" (baseURL is used there)
   await expect(page).toHaveURL('https://new-mentor-moi.vercel.app/about')
