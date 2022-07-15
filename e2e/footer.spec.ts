1b5722378c590e1759d20153b28eff367199a9bb
mentor-moi <109002782+mentor-moi@users.noreply.github.com>
2022-07-15 14:35:33 +0200

    Update footer.spec.ts

--- e2e/footer.spec.ts
+++ e2e/footer.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test('should navigate to the about page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
-  await page.goto('/')
+  await page.goto('https://new-mentor-moi.vercel.app/')
   // Find an element with the text 'About Page' and click on it
   //await  page.locator('span:has-text("Twitter")')
   // The new url should be "/about" (baseURL is used there)
