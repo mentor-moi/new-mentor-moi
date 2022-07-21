439cd1a1d8923d8c9684c472bad26a229ec4bdcb
mentor-moi <mentor.moi.22@gmail.com>
2022-07-21 22:06:23 +0200

    finish the homepage

--- components/elements/ImageContainerHero/imagecontainerhero.test.tsx
+++ components/elements/ImageContainerHero/imagecontainerhero.test.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import ReactDOM from 'react-dom'
+import { render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import ImageContainerHero from './ImageContainerHero'
 
 describe('HeroHomepage', () => {
-  it('renders a mentor text', () => {
-    render(<ImageContainerHero />)
-
-    const heading = screen.getByText(/Mentors/i)
-
-    expect(heading).toBeInTheDocument()
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(<ImageContainerHero />, div)
   })
 
-  it('Should have 14 images', () => {
+  it('Should have 11 images', () => {
     render(<ImageContainerHero />)
-    expect(document.querySelectorAll('.select-none').length).toBe(14)
+    expect(document.querySelectorAll('.select-none').length).toBe(11)
   })
 })
